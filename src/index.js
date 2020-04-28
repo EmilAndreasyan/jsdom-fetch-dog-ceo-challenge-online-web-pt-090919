fbfd0b668158b385c3b5476633f070e430a4cbaa
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-28 03:29:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('%c HI', 'color: firebrick')
 document.addEventListener("DOMContentLoaded", function(){
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const imgUrl = "https://dog.ceo/api/breeds/image/random"
   const dogImageContainer = document.getElementById("dog-image-container");
   fetch(imgUrl)
   .then(resp => resp.json())
